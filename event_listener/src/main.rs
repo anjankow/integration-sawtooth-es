71ac8a6f28f32e84ccb1850fe6711d1e0f794b7f
anjankow <anjankow@protonmail.com>
2023-04-21 09:57:25 +0000

    send message to validator

--- event_listener/src/main.rs
+++ event_listener/src/main.rs
@@ -1,10 +1,56 @@
+use protobuf::Message;
+
+extern crate protobuf;
 extern crate sawtooth;
+extern crate sawtooth_sdk;
 extern crate zmq;
 
+const DEFAULT_ZMQ_PRODUCER_ADDR: &str = "tcp://validator:4004";
+
 fn main() {
+    // get message producer address
+    let config = config::Config::builder()
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .separator("_")
+                .list_separator(" "),
+        )
+        .build()
+        .unwrap();
+    let producer_address = config
+        .get_string("APP_ZMQ_PRODUCER_ADDR")
+        .unwrap_or(DEFAULT_ZMQ_PRODUCER_ADDR.to_string());
+
+    // initialize zmq consumer
     let ctx = zmq::Context::new();
 
-    let socket = ctx.socket(zmq::REQ).unwrap();
-    socket.connect("tcp://127.0.0.1:1234").unwrap();
-    socket.send("hello world!", 0).unwrap();
+    let socket = ctx
+        .socket(zmq::DEALER)
+        .expect("Failed to create a zmq socket");
+    socket
+        .connect(&producer_address)
+        .expect("Failed to connect the socket");
+
+    // create subscription message
+    let event_type = "BATCH_COMMITED";
+    let subscription = sawtooth_sdk::messages::events::EventSubscription {
+        event_type: event_type.to_string(),
+        filters: protobuf::RepeatedField::from(vec![sawtooth_sdk::messages::events::EventFilter {
+            ..Default::default()
+        }]),
+
+        ..Default::default()
+    };
+    let event_subscribe_req = sawtooth_sdk::messages::client_event::ClientEventsSubscribeRequest {
+        subscriptions: protobuf::RepeatedField::from_vec(vec![subscription]),
+        ..Default::default()
+    };
+    // serialize it
+    let event_subscribe_req = event_subscribe_req
+        .write_to_bytes()
+        .expect("Failed to marshal event subcription request");
+
+    socket
+        .send(event_subscribe_req, 0)
+        .expect("Failed to send subscription message");
 }
